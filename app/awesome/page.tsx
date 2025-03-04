ad9b118a1a23e16a6d8814dc0d808ecda9e84164
Yobuligo <hoffmannpeter82@gmail.com>
2025-03-04 10:40:22 +0100

    Add debugging configuration

---
+++ app/awesome/page.tsx
@@ -0,0 +1,13 @@
+export default function AwesomePage() {
+  const test = () => {
+    console.log("Hello WOrld");
+  };
+
+  test();
+
+  return (
+    <main>
+      <h1>NextJs is awesome</h1>
+    </main>
+  );
+}
